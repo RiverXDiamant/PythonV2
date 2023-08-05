efb662cc2e34a97b6f5dee23fe3d71b345e20a37
Alex <108586058+RiverXDiamant@users.noreply.github.com>
2023-08-05 03:08:47 -0400

    added extra examples of using os modules

--- Course_2/Week_2.py
+++ Course_2/Week_2.py
@@ -130,3 +130,71 @@ with open("guests.txt","r") as guests:
 # Bob is checked in
 # Andrea is not checked in
 # We can see that Bob is checked in while Andrea is not. Nice work! You've learned the basics of reading and writing files in Python!
+
+# Examples
+
+# ======== 1 ========
+
+import os
+def create_python_script(filename):
+  comments = "# Start of a new Python program"
+  with open(filename, "a") as file:
+    filesize = file.write(comments)
+  return(filesize)
+
+print(create_python_script("program.py"))
+
+# 31
+
+# ======== 2 ========
+
+import os
+
+def new_directory(directory, filename):
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory)==False:
+    os.mkdir(directory)
+  # Create the new file inside of the new directory
+  path=os.path.join(directory,filename)
+  # Return the list of files in the new directory
+  os.mkdir(path)
+  return os.listdir(directory)
+  
+print(new_directory("PythonPrograms", "script.py"))
+
+# output: ['script.py']
+
+# ======== 3 ========
+
+import os
+def parent_directory():
+  # Create a relative path to the parent 
+  # of the current working directory 
+  path = os.getcwd()
+  relative_parent = os.path.abspath(os.path.join(path, os.pardir))
+
+  # Return the absolute path of the parent directory
+  return relative_parent
+
+print(parent_directory())
+
+# output: /
+
+# ======== 4 ========
+
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open(filename, "w+") as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  tm = datetime.datetime.fromtimestamp(timestamp).date()
+  # Convert the timestamp into a readable format, then into a string
+  
+  # Return just the date portion 
+  return ("{}".format(tm))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
