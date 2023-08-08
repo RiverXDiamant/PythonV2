5de67caad4d69baeb20b65dfcedfe7702ab0b5de
Alex <108586058+RiverXDiamant@users.noreply.github.com>
2023-08-08 09:54:11 -0400

    added a check punctuation function example

--- Course_2/Week_3.py
+++ Course_2/Week_3.py
@@ -44,3 +44,13 @@ def check_aei (text):
 print(check_aei("academia")) # True
 print(check_aei("aerial")) # False
 print(check_aei("paramedic")) # True
+
+def check_punctuation (text):
+  result = re.search(r"[,-.-!-;-?]", text)
+  return result != None
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
