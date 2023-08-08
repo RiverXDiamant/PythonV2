a1530bee26e4895a46689122a2a7b75f52a73f4f
Alex <108586058+RiverXDiamant@users.noreply.github.com>
2023-08-08 08:06:48 -0400

    added  regex example

--- Course_2/Week_3.py
+++ Course_2/Week_3.py
@@ -1,5 +1,15 @@
 # Regular Expressions
 
+import re
+
+# log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+# index = log.index("[")
+# print(log[index+1:index+6])
+
+# A more efficient way is to use regex to extract the process ID by
+# importing the re module which lets us use the search function to find regular expressions inside strings
+
 log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
-index = log.index("[")
-print(log[index+1:index+6])
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
