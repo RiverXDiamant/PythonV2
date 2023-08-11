a6e3445d6229fb4bde9d48333a61a23205b63202
Alex <108586058+RiverXDiamant@users.noreply.github.com>
2023-08-11 13:38:46 -0400

    completed testing example

--- Course_2/rearrange_test.py
+++ Course_2/rearrange_test.py
@@ -11,4 +11,15 @@ class TestRearrange(unittest.TestCase):
         testcase = ""
         expected = ""
         self.assertEqual(rearrange_name(testcase), expected)
+    
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Prince"
+        expected = "Prince"
+        self.assertEqual(rearrange_name(testcase), expected)
+
 unittest.main()
